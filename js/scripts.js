fffd89bd7792abef719c6cbe673c3770fe48466b
Brianca Knight <briancaknight@gmail.com>
2023-10-20 16:22:55 -0700

    Expand branches WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,55 @@
 // Buisness Logic
 
+
 function radio(event) {
   event.preventDefault();
   const shinySelect = document.querySelector("input[name='shinyDull']:checked").value;
-  // const petSelect = document.querySelector("input[name='petNoPet']:checked").value;
-  // const rSelect = document.querySelector("input[name='letter']:checked").value;
-  // const fabergeSelect = document.querySelector("input[name='faberge']:checked").value;
-  // const tasteSelect = document.querySelector("input[name='taste']:checked").value;
-  console.log(shinySelect)
-  // console.log(petSelect)
-  // console.log(rSelect)
-  // console.log(fabergeSelect)
-  // console.log(tasteSelect)
+  const petSelect = document.querySelector("input[name='petNoPet']:checked").value;
+  const rSelect = document.querySelector("input[name='letter']:checked").value;
+  const fabergeSelect = document.querySelector("input[name='faberge']:checked").value;
+  const tasteSelect = document.querySelector("input[name='taste']:checked").value;
+
+
+if (shinySelect === "yes" && petSelect === "no" && rSelect === "yes" && fabergeSelect === "yes" && tasteSelect === "yes"){
+  answer = "Ruby";
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+} else if (shinySelect === "yes" && petSelect === "yes" && rSelect === "no" && fabergeSelect === "yes" && tasteSelect === "no"){
+  answer = "Python";
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+} else if (shinySelect === "no" && petSelect === "no" && rSelect === "yes" && fabergeSelect === "no" && tasteSelect === "no"){
+  answer = "Rust";
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+} else if (petSelect === "no"){
+  answer = "Python"
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+} else if (tasteSelect === "yes"){
+  answer = "Ruby"
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+} else if (shinySelect === "no"){
+  answer = "Ruby"
+  document.getElementById("result").innerText = answer;
+  document.getElementById("resultDisplay").removeAttribute("class");
+
+// } else if (petSelect === "no"){
+//   answer = "Python"
+//   document.getElementById("result").innerText = answer;
+//   document.getElementById("resultDisplay").removeAttribute("class");
+// } else if (tasteSelect === "yes"){
+//   answer = "Ruby"
+//   document.getElementById("result").innerText = answer;
+//   document.getElementById("resultDisplay").removeAttribute("class");
+// } else if (shinySelect === "no"){
+//   answer = "Ruby"
+//   document.getElementById("result").innerText = answer;
+//   document.getElementById("resultDisplay").removeAttribute("class");
+// }
 }
 
-
-// User Interface logic
-
-let answer;
-  if (shinySelect == true){
-    answer = "Ruby";
-    document.getElementById("result").innerText = answer;
-    document.getElementById("resultDisplay").removeAttribute("class");
-  } else if (shinySelect === false){
-    answer = "Rust";
-    document.getElementById("result").innerText = answer;
-    document.getElementById("resultDisplay").removeAttribute("class");
-  }
-
-
-window.addEventListener("load", function () {
-  let form = document.querySelector("form");
-  form.addEventListener("submit", radio)
-})
-
-
-
-// // if (shinySelect && petSelect && rSelect && fabergeSelect && tasteSelect) {
-//   if ((shinySelect === true) && (petSelect === false) && (rSelect === true) && (fabergeSelect === true) && (tasteSelect === true)){
-//     answer = "Ruby";
-//     document.getElementById("result").innerText = answer;
-//   }
+window.addEventListener("load", function() {
+  document.getElementById("shinyDullForm").addEventListener("submit", radio);
+});
